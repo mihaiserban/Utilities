7f058d362779441b012922c449f8de85dde01958
mihaiserban <mihaiserban2@yahoo.com>
2013-04-16 15:24:59 +0300

    Create UILabel+MSVerticalAlign.h

---
+++ UILabel+MSVerticalAlign.h
@@ -0,0 +1,17 @@
+//
+//  UILabel+MSVerticalAlign
+//  Utility categories for iOS
+//
+//  Created by Mihai Serban on 4/16/13.
+//  Copyright (c) 2013 http://flavors.me/mihaiserban. All rights reserved.
+//  Distribuited under MIT License
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UILabel (MSVerticalAlign)
+
+- (void)alignTop;
+- (void)alignBottom;
+
+@end
