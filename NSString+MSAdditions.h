621a85c264d69751eafb7f69552dcb539e12d1c0
Serban Mihai <mihaiserban2@yahoo.com>
2013-04-16 15:46:21 +0300

    Create NSString+MSAdditions.h

---
+++ NSString+MSAdditions.h
@@ -0,0 +1,15 @@
+//
+//  NSString+MSAdditions
+//  Utility categories for iOS
+//
+//  Created by Mihai Serban on 4/16/13.
+//  Copyright (c) 2013 http://flavors.me/mihaiserban. All rights reserved.
+//  Distribuited under MIT License
+//
+#import <Foundation/Foundation.h>
+
+@interface NSString (MSAdditions)
+
+- (BOOL)containsString:(NSString *)subString;
+
+@end
