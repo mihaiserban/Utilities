a2f50650791d555d5779df5cc7586ff3cfce6326
mihaiserban <mihaiserban2@yahoo.com>
2013-04-16 15:24:03 +0300

    Create UIApplication+MSAdditions.h

---
+++ UIApplication+MSAdditions.h
@@ -0,0 +1,17 @@
+//
+//  UIApplication+MSAdditions.h
+//  Utility categories for iOS
+//
+//  Created by Mihai Serban on 4/16/13.
+//  Copyright (c) 2013 http://flavors.me/mihaiserban. All rights reserved.
+//  Distribuited under MIT License
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIApplication (MSAdditions)
+
++(id)appDelegate;
++(BOOL)isIpad;
+
+@end
