26ec7c0c2c5beb0d0fb5da76ffefba965bd50e59
mihaiserban <mihaiserban2@yahoo.com>
2013-04-16 14:58:55 +0300

    Create MSUIView+Layout.h

---
+++ MSUIView+Layout.h
@@ -0,0 +1,23 @@
+//
+//  MSUIView+Layout.h
+//  Utility categories for iOS
+//
+//  Created by Mihai Serban on 4/16/13.
+//  Copyright (c) 2013 http://flavors.me/mihaiserban. All rights reserved.
+//  Distribuited under MIT License
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (Layout)
+
+@property(assign) CGSize  size;
+@property(assign) CGPoint origin;
+@property(assign) CGFloat x;
+@property(assign) CGFloat y;
+@property(assign) CGFloat width;
+@property(assign) CGFloat height;
+
+-(void)removeAllSubviews;
+-(UIView*)viewWithTag:(NSUInteger)tag;
+@end
