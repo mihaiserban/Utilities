5cdf6b5d7476f9f42cf6e4a217f0a01903da29ed
mihaiserban <mihaiserban2@yahoo.com>
2013-04-16 15:25:14 +0300

    Create UILabel+MSVerticalAlign.m

---
+++ UILabel+MSVerticalAlign.m
@@ -0,0 +1,34 @@
+//
+//  UILabel+MSVerticalAlign
+//  Utility categories for iOS
+//
+//  Created by Mihai Serban on 4/16/13.
+//  Copyright (c) 2013 http://flavors.me/mihaiserban. All rights reserved.
+//  Distribuited under MIT License
+//
+
+#import "UILabel+MSVerticalAlign.h"
+
+@implementation UILabel (MSVerticalAlign)
+
+- (void)alignTop {
+    CGSize fontSize = [self.text sizeWithFont:self.font];
+    double finalHeight = fontSize.height * self.numberOfLines;
+    double finalWidth = self.frame.size.width;    //expected width of label
+    CGSize theStringSize = [self.text sizeWithFont:self.font constrainedToSize:CGSizeMake(finalWidth, finalHeight) lineBreakMode:self.lineBreakMode];
+    int newLinesToPad = (finalHeight  - theStringSize.height) / fontSize.height;
+    for(int i=0; i<newLinesToPad; i++)
+        self.text = [self.text stringByAppendingString:@"\n "];
+}
+
+- (void)alignBottom {
+    CGSize fontSize = [self.text sizeWithFont:self.font];
+    double finalHeight = fontSize.height * self.numberOfLines;
+    double finalWidth = self.frame.size.width;    //expected width of label
+    CGSize theStringSize = [self.text sizeWithFont:self.font constrainedToSize:CGSizeMake(finalWidth, finalHeight) lineBreakMode:self.lineBreakMode];
+    int newLinesToPad = (finalHeight  - theStringSize.height) / fontSize.height;
+    for(int i=0; i<newLinesToPad; i++)
+        self.text = [NSString stringWithFormat:@" \n%@",self.text];
+}
+
+@end
