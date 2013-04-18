3199864e00f5b49512d154bf21d1c88297951ddd
Serban Mihai <mihaiserban2@yahoo.com>
2013-04-19 02:38:41 +0300

    Update UILabel+MSVerticalAlign.m

--- UILabel/UILabel+MSVerticalAlign.m
+++ UILabel/UILabel+MSVerticalAlign.m
@@ -8,27 +8,64 @@
 //
 
 #import "UILabel+MSVerticalAlign.h"
+#import <objc/runtime.h>
 
 @implementation UILabel (MSVerticalAlign)
 
-- (void)alignTop {
-    CGSize fontSize = [self.text sizeWithFont:self.font];
-    double finalHeight = fontSize.height * self.numberOfLines;
-    double finalWidth = self.frame.size.width;    //expected width of label
-    CGSize theStringSize = [self.text sizeWithFont:self.font constrainedToSize:CGSizeMake(finalWidth, finalHeight) lineBreakMode:self.lineBreakMode];
-    int newLinesToPad = (finalHeight  - theStringSize.height) / fontSize.height;
-    for(int i=0; i<newLinesToPad; i++)
-        self.text = [self.text stringByAppendingString:@"\n "];
+-(UITextVerticalAlignment)textVerticalAlignment
+{
+    NSNumber *alignment = objc_getAssociatedObject(self, "UITextVerticalAlignment");
+
+    if (alignment)
+    {
+        return [alignment intValue];
+    }
+
+    NSNumber *newAlignment = [NSNumber numberWithInt:1];
+    objc_setAssociatedObject(self, "UITextVerticalAlignment", newAlignment, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+    return UITextVerticalAlignmentMiddle;
 }
 
-- (void)alignBottom {
-    CGSize fontSize = [self.text sizeWithFont:self.font];
-    double finalHeight = fontSize.height * self.numberOfLines;
-    double finalWidth = self.frame.size.width;    //expected width of label
-    CGSize theStringSize = [self.text sizeWithFont:self.font constrainedToSize:CGSizeMake(finalWidth, finalHeight) lineBreakMode:self.lineBreakMode];
-    int newLinesToPad = (finalHeight  - theStringSize.height) / fontSize.height;
-    for(int i=0; i<newLinesToPad; i++)
-        self.text = [NSString stringWithFormat:@" \n%@",self.text];
+-(void)setTextVerticalAlignment:(UITextVerticalAlignment)textVerticalAlignment
+{
+    NSNumber *newAlignment = [NSNumber numberWithInt:textVerticalAlignment];
+    objc_setAssociatedObject(self, "UITextVerticalAlignment", newAlignment, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+
+    [self setNeedsDisplay];
+}
+
+-(CGRect)verticalAlignTextRectForBounds:(CGRect)bounds limitedToNumberOfLines:(NSInteger)numberOfLines
+{
+    CGRect textRect = [self verticalAlignTextRectForBounds:bounds limitedToNumberOfLines:numberOfLines];
+
+    switch ([self textVerticalAlignment])
+    {
+        case UITextVerticalAlignmentTop:
+            textRect.origin.y = bounds.origin.y;
+            break;
+
+        case UITextVerticalAlignmentBottom:
+            textRect.origin.y = bounds.origin.y + bounds.size.height - textRect.size.height;
+            break;
+
+        case UITextVerticalAlignmentMiddle:
+            textRect.origin.y = bounds.origin.y + (bounds.size.height - textRect.size.height) / 2.0;
+            break;
+    }
+
+    return textRect;
+}
+
+-(void)verticalAlignDrawTextInRect:(CGRect)rect
+{
+    CGRect actualRect = [self textRectForBounds:rect limitedToNumberOfLines:self.numberOfLines];
+    [self verticalAlignDrawTextInRect:actualRect];
+}
+
++(void)load
+{
+    method_exchangeImplementations(class_getInstanceMethod(self, @selector(textRectForBounds:limitedToNumberOfLines:)), class_getInstanceMethod(self, @selector(verticalAlignTextRectForBounds:limitedToNumberOfLines:)));
+    method_exchangeImplementations(class_getInstanceMethod(self, @selector(drawTextInRect:)), class_getInstanceMethod(self, @selector(verticalAlignDrawTextInRect:)));
 }
 
 @end
